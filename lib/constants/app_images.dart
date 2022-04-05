26f73ab367ca292734adf935434ca6671e9a85e7
Olaoluwa1995 <olakulehinstephen@gmail.com>
2022-04-05 16:53:53 +0100

    Created app images

---
+++ lib/constants/app_images.dart
@@ -0,0 +1,19 @@
+class AppImages {
+  // ICONS
+  static const String bedtime = 'assets/icons/bedtime.png';
+  static const String bottle = 'assets/icons/bottle.png';
+  static const String call = 'assets/icons/call.png';
+  static const String donate = 'assets/icons/donate.png';
+  static const String walk = 'assets/icons/walk.png';
+  static const String leaf = 'assets/icons/leaf.png';
+  static const String bulb = 'assets/icons/bulb.png';
+  static const String star = 'assets/icons/star.png';
+  static const String sun = 'assets/icons/sun.png';
+  static const String coloredLeaf = 'assets/icons/colored_leaf.png';
+  static const String flash = 'assets/icons/flash.png';
+  static const String coloredFlash = 'assets/icons/colored_flash.png';
+
+  // IMAGES
+  static const String clock1 = 'assets/images/clock1.png';
+  static const String clock2 = 'assets/images/clock2.png';
+}
