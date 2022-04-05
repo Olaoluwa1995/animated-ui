e69e3aaea2caefbd05663f5e1e84e79defa208a1
Olaoluwa1995 <olakulehinstephen@gmail.com>
2022-04-05 16:53:47 +0100

    Created app fonts

---
+++ lib/constants/app_fonts.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class AppFonts {
+  static TextStyle text = GoogleFonts.manrope(
+    textStyle: const TextStyle(
+      fontSize: 14,
+      fontWeight: FontWeight.w500,
+      color: Color(0xFF939295),
+    ),
+  );
+}
